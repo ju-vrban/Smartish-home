5fa54553c61aac8a90c71702ed26ec690a6d2f11
Jurica <vrbanec.jura.jurica@gmail.com>
2020-01-28 17:21:59 +0100

    Diplomski rad

---
+++ Debug/src/subdir.mk
@@ -0,0 +1,39 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+################################################################################
+
+# Add inputs and outputs from these tool invocations to the build variables 
+C_SRCS += \
+../src/_write.c \
+../src/main.c \
+../src/stm32f4xx_hal_msp.c \
+../src/stm32f4xx_it.c \
+../src/syscalls.c \
+../src/system_stm32f4xx.c 
+
+OBJS += \
+./src/_write.o \
+./src/main.o \
+./src/stm32f4xx_hal_msp.o \
+./src/stm32f4xx_it.o \
+./src/syscalls.o \
+./src/system_stm32f4xx.o 
+
+C_DEPS += \
+./src/_write.d \
+./src/main.d \
+./src/stm32f4xx_hal_msp.d \
+./src/stm32f4xx_it.d \
+./src/syscalls.d \
+./src/system_stm32f4xx.d 
+
+
+# Each subdirectory must supply rules for building sources it contributes
+src/%.o: ../src/%.c
+	@echo 'Building file: $<'
+	@echo 'Invoking: GNU ARM Cross C Compiler'
+	arm-none-eabi-gcc -mcpu=cortex-m4 -mthumb -Og -fmessage-length=0 -fsigned-char -ffunction-sections -fdata-sections -ffreestanding -fno-move-loop-invariants -Wall -Wextra  -g3 -DDEBUG -DSTM32F429xx -DTRACE -DOS_USE_TRACE_SEMIHOSTING_DEBUG -I"../include" -I"../system/include" -I"../system/include/cmsis" -I"../system/include/stm32f4xx" -std=gnu11 -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@)" -c -o "$@" "$<"
+	@echo 'Finished building: $<'
+	@echo ' '
+
+
---
+++ Debug/system/src/cmsis/subdir.mk
@@ -0,0 +1,24 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+################################################################################
+
+# Add inputs and outputs from these tool invocations to the build variables 
+S_UPPER_SRCS += \
+../system/src/cmsis/startup_stm32f429xx.S 
+
+OBJS += \
+./system/src/cmsis/startup_stm32f429xx.o 
+
+S_UPPER_DEPS += \
+./system/src/cmsis/startup_stm32f429xx.d 
+
+
+# Each subdirectory must supply rules for building sources it contributes
+system/src/cmsis/%.o: ../system/src/cmsis/%.S
+	@echo 'Building file: $<'
+	@echo 'Invoking: GNU ARM Cross Assembler'
+	arm-none-eabi-gcc -mcpu=cortex-m4 -mthumb -Og -fmessage-length=0 -fsigned-char -ffunction-sections -fdata-sections -ffreestanding -fno-move-loop-invariants -Wall -Wextra  -g3 -x assembler-with-cpp -DDEBUG -DTRACE -DOS_USE_TRACE_SEMIHOSTING_DEBUG -I"../include" -I"../system/include" -I"../system/include/cmsis" -I"../system/include/stm32f4xx" -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@)" -c -o "$@" "$<"
+	@echo 'Finished building: $<'
+	@echo ' '
+
+
